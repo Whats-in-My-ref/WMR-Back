53c2333260cbf5b442b5b38fd9573b5ef772da28
mink <frog050406@gmail.com>
2025-02-27 18:51:13 +0900

    feat/Project File

---
+++ src/main/java/com/project/wmr/WmrApplication.java
@@ -0,0 +1,13 @@
+package com.project.wmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/wmr/WmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
